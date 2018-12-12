0cfaed26707321f013f1d454409642863c4588cc
Rania29 <37171434+Rania29@users.noreply.github.com>
2018-12-12 20:07:42 +0300

    12-Dec-18-20:07:41.59

--- src/main/webapp/sw.js
+++ src/main/webapp/sw.js
@@ -5,6 +5,30 @@
  */
 /*self.addEventListener('fetch', (event) => {});*/
 
-self.addEventListener("fetch", function(event){
+importScripts('/cache-polyfill.js');
 
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.xhtml',
+       '/index.xhtml?homescreen=1',
+       '/?homescreen=1',
+       '/resources/indexcss.css',
+       /*'/javascript/main.min.js',*/
+      /* '/sounds/airhorn.mp3'*/
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
 });
