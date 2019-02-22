29725e05d9b959a636f7ec36983c2aef106e6ea0
Rania29 <37171434+Rania29@users.noreply.github.com>
2019-02-22 19:18:15 +0300

    22-Feb-19-19:18:14.51

--- src/main/webapp/resources/javascript/slideshowscript.js
+++ src/main/webapp/resources/javascript/slideshowscript.js
@@ -43,3 +43,29 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " w3-white";
 }
+
+/***************************** Star Rating ******************************************/
+
+var count;
+        function starmark(item)
+        {
+            count = item.id[0];
+            sessionStorage.starRating = count;
+            var subid = item.id.substring(1);
+            for (var i = 0; i < 5; i++)
+            {
+                if (i < count)
+                {
+                    document.getElementById((i + 1) + subid).style.color = "orange";
+                } else
+                {
+                    document.getElementById((i + 1) + subid).style.color = "black";
+                }
+            }
+        }
+        function result()
+        {
+//Rating : Count
+//Review : Comment(id)
+            alert("Rating : " + count + "\nReview : " + document.getElementById("comment").value);
+        }
